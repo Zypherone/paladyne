0774c8968507dc8cd5fb894d3d606e01bdb438d6
Zypherone <360494+Zypherone@users.noreply.github.com>
2023-08-09 00:21:46 +1000

    First Push of DEV site

---
+++ server.js
@@ -0,0 +1,20 @@
+//const http = require('http');
+const express = require('express');
+//const path = require('path');
+const app = express();
+//app.use(express.json());
+//app.use(express.static("express"));
+// default URL for website
+
+/*
+app.use('/', function(req,res){
+    res.sendFile(path.join(__dirname+'/index.html'));
+    //__dirname : It will resolve to your project folder.
+  });
+*/
+app.use(express.static('public'));
+
+//const server = http.createServer(app);
+const port = 3000;
+app.listen(port);
+console.debug('Server listening on port http://localhost:' + port);
